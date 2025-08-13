45b089d20363dc8ac68a691ef86fd1a2fa5aa877
Xabier Jauregi <giamexak@gmail.com>
2025-08-13 10:02:38 +0200

    controlled and uncontrolled components

---
+++ section-4/src/App.jsx
@@ -0,0 +1,13 @@
+import { ControlledForm } from "./components/controlled-form";
+import { UncontrolledForm } from "./components/uncontrolled-form";
+
+function App() {
+  return (
+    <>
+      <UncontrolledForm />
+      <ControlledForm />
+    </>
+  );
+}
+
+export default App;
